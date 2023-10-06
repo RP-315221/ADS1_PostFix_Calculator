31f84e7580d7dedf5d323d404154a5e501f3fe5e
Rojus Paukste <109368084+RyzhasMomentas@users.noreply.github.com>
2023-10-06 17:08:57 +0300

    Updated System to take input from the user
    The system will now ask for a users input as an expression. Moreover after the expression is calculated, the system will also ask if a user desires to calculate a new expression.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,59 +3,40 @@ package org.example;
 import org.example.domain.*;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Token a=new Operand(1);
-        Token b=new Operand(2);
-        Token c=new Operator("+");
-        Token d=new Operand(5);
-        Token e=new Operator("*");
-
-        Token f=new Operand(10);
-        Token g=new Operand(1);
-        Token h=new Operator("-");
-        Token i=new Operand(3);
-        Token j=new Operator("/");
-
-        Token k=new Operand(5);
-        Token l=new Operand(2);
-        Token m=new Operator("%");
-        Token n=new Operand(3);
-        Token o=new Operator("+");
-
-        ArrayList<Token> tokenList1=new ArrayList<>();
-        ArrayList<Token> tokenList2=new ArrayList<>();
-        ArrayList<Token> tokenList3=new ArrayList<>();
-
-        tokenList1.add(a);
-        tokenList1.add(b);
-        tokenList1.add(c);
-        tokenList1.add(d);
-        tokenList1.add(e);
-
-        tokenList2.add(f);
-        tokenList2.add(g);
-        tokenList2.add(h);
-        tokenList2.add(i);
-        tokenList2.add(j);
-
-        tokenList3.add(k);
-        tokenList3.add(l);
-        tokenList3.add(m);
-        tokenList3.add(n);
-        tokenList3.add(o);
-
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter the expression:");
+        String expression=scanner.nextLine();
+        ArrayList<Token> tokenList=new ArrayList<>();
+        String[] tokens=expression.split(" ");
+        for(String token:tokens)
+        {
+            if(token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/") || token.equals("%"))
+            {
+                tokenList.add(new Operator(token));
+            }
+            else
+            {
+                tokenList.add(new Operand(Integer.parseInt(token)));
+            }
+        }
         Calculator calculator=new CalculatorImpl();
         try
         {
-            System.out.println(calculator.evaluateExpression(tokenList1));
-            System.out.println(calculator.evaluateExpression(tokenList2));
-            System.out.println(calculator.evaluateExpression(tokenList3));
+            System.out.println(calculator.evaluateExpression(tokenList));
         }
         catch(Exception error)
         {
             System.out.println(error.getMessage());
         }
+        System.out.println("New expression? (y/n)");
+        String choice=scanner.nextLine();
+        if(choice.equals("y"))
+            main(args);
+        else
+            System.exit(0);
     }
 }
